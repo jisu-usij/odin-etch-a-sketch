4c61431821193ff312e4d6fa7ef5e2067319ccf7
jisu-usij <133517588+jisu-usij@users.noreply.github.com>
2023-07-02 10:01:10 -0400

    Make grid creation dynamic
    Now a grid of any size n x n can be created, instead
    of each square being hardcoded to a certain size.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,14 @@ const container = document.querySelector('.grid-container');
 
 // Add squares to the grid
 for (let i = 0; i < 16; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  container.appendChild(square);
+  const column = document.createElement('div');
+  column.classList.add('column');
+
+  for (let i = 0; i < 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    column.appendChild(square);
+  }
+
+  container.appendChild(column);
 }
