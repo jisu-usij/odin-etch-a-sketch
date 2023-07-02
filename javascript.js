47a813ddcd248550c1fb15855d8bfe928903fadb
jisu-usij <133517588+jisu-usij@users.noreply.github.com>
2023-07-02 19:37:25 -0400

    Add additional features
    Added a button to clear the canvas.
    
    Also added buttons to switch between normal mode
    (drawing in black) and rainbow mode (drawing in random
    colors).

--- javascript.js
+++ javascript.js
@@ -26,23 +26,26 @@ function addHoverEffect(effectFunction) {
   squares.forEach(square => square.addEventListener('mouseenter', effectFunction));
 }
 
+function removeHoverEffect(effectFunction) {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.removeEventListener('mouseenter', effectFunction));
+}
+
 function colorSquareBlack() {
   this.style.backgroundColor = 'black';
+}
+
+function colorSquareRainbow() {
+  this.setAttribute('style', `background-color: rgb(${randomRGBValue()}, ${randomRGBValue()}, ${randomRGBValue()})`);
+}
+
+function randomRGBValue() {
+  return Math.floor(Math.random() * 256);
 }
 
 function removeGrid() {
   const container = document.querySelector('.grid-container');
   container.remove();  
-}
-
-function setUpSizeButton() {
-  const sizeButton = document.querySelector('button.size');
-  sizeButton.addEventListener('click', () => {
-    const size = sizeFromUser();
-    if (size === null) return;
-    removeGrid();
-    createGrid(size);
-  });
 }
 
 function sizeFromUser() {
@@ -58,8 +61,57 @@ function sizeFromUser() {
   }
 }
 
+function setUpSizeButton() {
+  const sizeButton = document.querySelector('button.size');
+  sizeButton.addEventListener('click', () => {
+    const size = sizeFromUser();
+    if (size === null) return;
+    removeGrid();
+    createGrid(size);
+  });
+}
+
+function setUpNormalButton() {
+  const normalButton = document.querySelector('button.normal');
+  normalButton.addEventListener('click', () => {
+    // Can maybe improve performance by checking
+    // if the event handler already exists instead
+    // of trying to remove the event handlers
+    // regardless of whether or not they already exist.
+    removeHoverEffect(colorSquareBlack);
+    removeHoverEffect(colorSquareRainbow);
+
+    addHoverEffect(colorSquareBlack);
+  });
+}
+
+function setUpRainbowButton() {
+  const rainbowButton = document.querySelector('button.rainbow');
+  rainbowButton.addEventListener('click', () => {
+    // Can maybe improve performance by checking
+    // if the event handler already exists instead
+    // of trying to remove the event handlers
+    // regardless of whether or not they already exist.
+    removeHoverEffect(colorSquareBlack);
+    removeHoverEffect(colorSquareRainbow);
+
+    addHoverEffect(colorSquareRainbow);
+  });
+}
+
+function setUpClearButton() {
+  const clearButton = document.querySelector('button.clear');
+  clearButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.style.cssText='background-color: cornsilk');
+  });
+}
+
 function setUpButtons() {
   setUpSizeButton();
+  setUpNormalButton();
+  setUpRainbowButton();
+  setUpClearButton();
 }
 
 createGrid(16);
